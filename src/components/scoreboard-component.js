1efb6a4a18d568178fdd0b20d4fced949d93efb3
johngibbs15 <john.gibbs901@gmail.com>
2023-02-27 12:06:19 -0500

    Update

---
+++ src/components/scoreboard-component.js
@@ -0,0 +1,9 @@
+const scoreboard = ({ score }) => {
+    return (
+        <div className="score-board">
+            <h2>Score: {score}</h2>
+        </div>
+    );
+};
+
+export default scoreboard;
